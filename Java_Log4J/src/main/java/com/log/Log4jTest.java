ea6fe32d6a4bdace3a4cbd7f8152bca2216e49cf
leizuquan <843291011@qq.com>
2020-11-25 20:22:59 +0800

    # 新增：Log4j 的组件和配置文件搭配

--- Java_Log4J/src/main/java/com/log/Log4jTest.java
+++ Java_Log4J/src/main/java/com/log/Log4jTest.java
@@ -1,6 +1,5 @@
 package com.log;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
@@ -17,7 +16,7 @@ public class Log4jTest {
     @Test
     public void testQuick(){
         // 初始化配置信息，在入门案例中暂不使用配置文件
-        BasicConfigurator.configure();
+        // BasicConfigurator.configure();
         // 获取日志记录器对象
         Logger logger = Logger.getLogger(Log4jTest.class);
         // 日志记录输出
