aa93f544176a8724f271bca0765da4a2c4228702
leizuquan <843291011@qq.com>
2020-11-25 19:13:05 +0800

    # 新增：Log4j 基础

---
+++ Java_Log4J/src/main/java/com/log/Log4jTest.java
@@ -0,0 +1,34 @@
+package com.log;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+/**
+ * @Author:
+ * @Date: 2020-11-25 18:56
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+public class Log4jTest {
+
+    // Log4j 基础
+    @Test
+    public void testQuick(){
+        // 初始化配置信息，在入门案例中暂不使用配置文件
+        BasicConfigurator.configure();
+        // 获取日志记录器对象
+        Logger logger = Logger.getLogger(Log4jTest.class);
+        // 日志记录输出
+        logger.info("hello log4j");
+
+        // 日志级别有6种，默认的日志级别是DEBUG
+        logger.fatal("fatal"); // 严重错误，一般会造成系统崩溃并终止运行
+        logger.error("error"); // 错误信息，不会影响系统运行
+        logger.warn("warn");   // 警告信息，可能会发生问题
+        logger.info("info");   // 运行信息，数据连接、网络连接、IO 操作等等
+        logger.debug("debug"); // 调试信息，一般在开发中使用，记录程序变量参数传递信息等等
+        logger.trace("trace"); // 追踪信息，记录程序所有的流程信息
+    }
+}
