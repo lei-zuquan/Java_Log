cff48362bf2ed62779d8f4394fb7f30a108c02e3
leizuquan1990 <843291011@qq.com>
2020-11-27 13:52:56 +0800

    # 新增：slf4j 入门，通过slf4j门面 + slf4j-simple具体实现类 来实现日志管理

---
+++ T04_Java_SLF4J/src/main/java/Slf4jTest.java
@@ -0,0 +1,67 @@
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Author:
+ * @Date: Created in 11:46 上午 2020/11/27
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+public class Slf4jTest {
+
+    // 为了保证使用时，不需要每次都去创建logger 对象，我们声明静态常量
+    public static final Logger LOGGER = LoggerFactory.getLogger(Slf4jTest.class);
+
+    // 快速入门
+    @Test
+    public void testQuick(){
+        // 日志输出
+        LOGGER.error("error");
+        LOGGER.warn("warning");
+        LOGGER.info("info");    // 默认的日志级别信息
+        LOGGER.debug("debug");
+        LOGGER.trace("trace");  // 追踪信息
+
+        // 使用占位符输出日志信息
+        String name = "java_log";
+        Integer age = 18;
+        LOGGER.info("用户：{}，{}", name, age);
+
+        // 将系统的异常信息输出
+        try {
+            int i = 1 / 0;
+        } catch (Exception e){
+            // e.printStackTrace();
+            LOGGER.error("出现异常：", e);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
