9e539d0424bdfdf93901eda30c326988b1229897
leizuquan1990 <843291011@qq.com>
2020-12-01 21:08:17 +0800

    # slf4j日志：通过logback 将日志输出到控制台

---
+++ T05_LogBack/src/main/java/com/log/LogbackTest.java
@@ -0,0 +1,27 @@
+package com.log;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Author:
+ * @Date: 2020-12-01 20:30
+ * @Version: 1.0
+ * @Modified By:
+ * @Description:
+ */
+public class LogbackTest {
+    public static final Logger LOGGER = LoggerFactory.getLogger(LogbackTest.class);
+
+    // 快速入门
+    @Test
+    public void testQuick() throws Exception {
+        // 日志输出
+        LOGGER.error("error");
+        LOGGER.warn("warning");
+        LOGGER.info("info");
+        LOGGER.debug("debug");  // 默认的日志级别信息
+        LOGGER.trace("trace");  // 追踪信息
+    }
+}
