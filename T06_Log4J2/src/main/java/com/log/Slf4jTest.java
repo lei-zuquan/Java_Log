f392c436b67be7001a8f111eef73f1c2c009d5e3
leizuquan1990 <843291011@qq.com>
2020-12-06 12:19:11 +0800

    # log4j2日志：AsyncAppender异步日志输出，提升效率。这种方式使用得不多

--- T06_Log4J2/src/main/java/com/log/Slf4jTest.java
+++ T06_Log4J2/src/main/java/com/log/Slf4jTest.java
@@ -20,14 +20,17 @@ public class Slf4jTest {
     @Test
     public void testQuick(){
         // 日志输出
-        LOGGER.error("error");
-        LOGGER.warn("warning");
-        LOGGER.info("info");    // 默认的日志级别信息
-        LOGGER.debug("debug");
-        LOGGER.trace("trace");  // 追踪信息
+        //for (int i = 0; i < 1000000; i++) {
+            LOGGER.error("error");
+            LOGGER.warn("warning");
+            LOGGER.info("info");    // 默认的日志级别信息
+            LOGGER.debug("debug");
+            LOGGER.trace("trace");  // 追踪信息
+        //}
+
 
         // 使用占位符输出日志信息
-        String name = "java_log";
+        /*String name = "java_log";
         Integer age = 18;
         LOGGER.info("用户：{}，{}", name, age);
 
@@ -37,7 +40,7 @@ public class Slf4jTest {
         } catch (Exception e){
             // e.printStackTrace();
             LOGGER.error("出现异常：", e);
-        }
+        }*/
     }
 }
 
